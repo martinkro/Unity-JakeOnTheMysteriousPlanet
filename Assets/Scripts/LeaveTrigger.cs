8edc1e64b5228ce45c2d4c836a3c1e083c3e5792
chameleon <martinkro@aliyun.com>
2017-04-02 21:00:30 +0800

    Game Level

---
+++ Assets/Scripts/LeaveTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeaveTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		Debug.Log ("LeaveTrigger position - enter: " + other.transform.position);
+		LevelGenerator.instance.AddPiece ();
+		LevelGenerator.instance.RemoveOldestPiece ();
+
+	}
+
+	void OnTriggerLeave2D(Collider2D other){
+		Debug.Log ("LeaveTrigger position - leave: " + other.transform.position);
+
+	}
+}
---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour {
+
+	public static LevelGenerator instance;
+	public List<LevelPiece> levelPrefabs = new List<LevelPiece>();
+	public Transform levelStartPoint;
+	public List<LevelPiece> pieces = new List<LevelPiece> ();
+
+	void Awake(){
+		instance = this;
+	}
+	// Use this for initialization
+	void Start () {
+		GenerateInitialPieces ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void GenerateInitialPieces(){
+		for (int i = 0; i < 3; i++) {
+			AddPiece ();
+		}
+	}
+
+	public void AddPiece(){
+		int randomIndex = Random.Range (0, levelPrefabs.Count);
+		LevelPiece piece = (LevelPiece)Instantiate (levelPrefabs [randomIndex]);
+		piece.transform.SetParent (this.transform, false);
+
+		Vector3 spawnPosition = Vector3.zero;
+		if (pieces.Count == 0) {
+			spawnPosition = levelStartPoint.position;
+		} else {
+			spawnPosition = pieces [pieces.Count - 1].exitPoint.position;
+		}
+
+		piece.transform.position = spawnPosition;
+		pieces.Add (piece);
+	}
+
+	public void RemoveOldestPiece(){
+		LevelPiece oldestPiece = pieces [0];
+		pieces.Remove (oldestPiece);
+		Destroy (oldestPiece.gameObject);
+	}
+}
